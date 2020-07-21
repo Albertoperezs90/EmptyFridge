d97ab47d77abad92f3e1a32b06c887ea007de33f
Alberto Pérez <alberto.perez@innocv.com>
2020-07-21 10:29:24 +0200

    Ignore .idea folder and update gradle

---
+++ app/src/main/java/com/aperezsi/emptyfridge/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.aperezsi.emptyfridge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity(private val layoutId: Int) : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutId)
+    }
+}
